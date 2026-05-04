8e3b46ed1694366ea807c7b0621932b63e5194a3
Jeongsu Yang <mycat83@gmail.com>
2026-05-04 11:57:05 +0900

    fix: baseUrl을 my-notes로 변경

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "jsyang-dev.github.io/obsidian-notes",
+    baseUrl: "jsyang-dev.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
